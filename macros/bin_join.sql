585aac23d52a206562e924962ccd1165a6134e7b
bchaoss <103650247+bchaoss@users.noreply.github.com>
2026-06-03 09:46:38 +0800

    add lower-bound bin "<threshold" for values below first threshold

--- macros/bin_join.sql
+++ macros/bin_join.sql
@@ -1,10 +1,17 @@
 {% macro bin_join(value, bins, bins_alias='bins') %}
 
 left join {{ bins }} as {{ bins_alias }}
-    on {{ value }} >= {{ bins_alias }}.bin_start
-    and (
-        {{ value }} < {{ bins_alias }}.bin_end
-        or {{ bins_alias }}.bin_end is null
+    on (
+        {{ bins_alias }}.bin_start is null
+        and {{ value }} < {{ bins_alias }}.bin_end
+    )
+    or (
+        {{ bins_alias }}.bin_start is not null
+        and {{ value }} >= {{ bins_alias }}.bin_start
+        and (
+            {{ value }} < {{ bins_alias }}.bin_end
+            or {{ bins_alias }}.bin_end is null
+        )
     )
 
 {% endmacro %}
--- macros/generate_bins.sql
+++ macros/generate_bins.sql
@@ -11,11 +11,17 @@ with thresholds as (
 bins as (
 
     select
+        cast(null as {{ dbt.type_int() }}) as bin_start,
+        min(threshold) as bin_end
+    from thresholds
+
+    union all
+
+    select
         threshold as bin_start,
         lead(threshold) over (
             order by threshold
         ) as bin_end
-
     from thresholds
 
 )
@@ -24,6 +30,9 @@ select
     bin_start,
     bin_end,
     case
+        when bin_start is null
+        then '<' || cast(bin_end as {{ dbt.type_string() }})
+
         when bin_end is null
         then cast(bin_start as {{ dbt.type_string() }}) || '+'
 
